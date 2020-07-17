d335d7fc2d13d9065239a52a6bff506ea897e4bf
jackyzha0 <j.zhao2k19@gmail.com>
2020-07-17 00:23:19 -0700

    refactor to use memoization

--- src/apps/Call.js
+++ src/apps/Call.js
@@ -1,10 +1,12 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useContext } from "react";
 import Webcam from "react-webcam";
+import UserContext from "../contexts/UserContext";
 import { PeerConnection } from "../websockets/Connection";
 
 const WebcamComponent = () => <Webcam />;
 
-const Call = ({ user }) => {
+const Call = () => {
+    const user = useContext(UserContext);
     useMemo(() => new PeerConnection(user), [user]);
     return null;
 };
--- src/apps/SessionInitiator.js
+++ src/apps/SessionInitiator.js
@@ -1,12 +1,11 @@
-import React, { useContext, useMemo } from "react";
+import React, { useMemo, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { PeerConnection } from "../websockets/Connection";
 import UserContext from "../contexts/UserContext";
 
 export default () => {
-    const { userId } = useParams();
     const user = useContext(UserContext);
-
+    const { userId } = useParams();
     useMemo(() => new PeerConnection(user, true, userId), [user, userId]);
     return (
         <>
