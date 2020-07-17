e3a2cdcbb5fb066c655c8062324fcbe802c7d5ca
Yashika Sharma <sharmayashika996@gmail.com>
2020-07-17 12:13:15 +0530

    Tried to replicate Nick's layout  lol

--- src/apps/OfficeView.js
+++ src/apps/OfficeView.js
@@ -1,9 +1,43 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { Card } from "../styled/Card";
+import { Button } from "../components/Inputs";
 
-
-export default function() {
+export default function () {
     const { officeId } = useParams();
 
-    return <p>Hello and welcome to {officeId}</p>;
+    return (
+        <Card className="lt-card lt-shadow">
+            <div class="Office">
+                <p className="lt-card lt-hover text-align:left">
+                    <b>Office Name</b>
+                    <Button className="lt-button lt-hover  --lt-colours-lightgray: #f0f0f0">
+                        Leave
+                    </Button>
+                </p>
+            </div>
+
+            <div class="Desk">
+                <p className="lt-card lt-hover text-align:left">
+                    <b>John D's Desk</b>
+                    <Button className="lt-button lt-hover  --lt-colours-secondary: #84a59d">
+                        Pull Up
+                    </Button>{" "}
+                </p>
+                <p className="lt-card lt-hover text-align:left">
+                    <b>Doanas's Desk</b>
+                    <Button className="lt-button lt-hover  --lt-colours-secondary: #84a59d">
+                        Pull Up
+                    </Button>{" "}
+                </p>
+
+                <p className="lt-card lt-hover text-align:left">
+                    <b>Mark's Desk</b>
+                    <Button className="lt-button lt-hover  --lt-colours-secondary: #84a59d">
+                        Pull Up
+                    </Button>
+                </p>
+            </div>
+        </Card>
+    );
 }
