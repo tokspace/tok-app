a4cf74a72af5c0b8fa7dc2363bd2247fa92facce
Yashika Sharma <sharmayashika996@gmail.com>
2020-07-17 14:25:45 +0530

    new branch

--- src/apps/OfficeView.js
+++ src/apps/OfficeView.js
@@ -32,26 +32,36 @@ export default function () {
     };
 
     return (
-        <Card>
-            <h1>{office.Name}</h1>
-            <ul>
-                {office.Users.map((user) => (
-                    <li key={user.user.id}>
-                        <Link to={`/sessions/new-with-user/${user.user.id}`}>
-                            {user.name}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-            <Button to="/" className="lt-button lt-hover" onClick={signout}>
-                Signout
-            </Button>
-            <Button
-                to="/settings"
-                className="lt-button lt-hover"
-                onClick={() => history.push("/settings")}>
-                Settings
-            </Button>
+        <Card className="lt-card lt-shadow">
+            <div class="Office">
+                <p className="lt-card lt-hover text-align:left">
+                    <b>Office Name{office.Name}</b>
+                    <Button className="lt-button lt-hover  --lt-colours-lightgray: #f0f0f0">
+                        Leave
+                    </Button>
+                </p>
+            </div>
+
+            <div class="Desk">
+                <p className="lt-card lt-hover text-align:left">
+                    <ul>
+                        {office.Users.map((user) => (
+                            <li key={user.user.id}>
+                                <Link
+                                    to={`/sessions/new-with-user/${user.user.id}`}>
+                                    {user.name}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                    <Button
+                        to="/"
+                        className="lt-button lt-hover  --lt-colours-secondary: #84a59d"
+                        onClick={signout}>
+                        Pull Up
+                    </Button>{" "}
+                </p>
+            </div>
         </Card>
     );
 }
